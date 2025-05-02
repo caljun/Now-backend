3f9e3ad4bf1ff4b6326db7d9423ad1b25ad0130f
caljun <calderonjunya0602@gmail.com>
2025-05-02 22:38:16 +0900

    Create Area.js

---
+++ models/Area.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const areaSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  coords: [
+    {
+      lat: Number,
+      lng: Number
+    }
+  ],
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Area', areaSchema);
