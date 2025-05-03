35646732096a1b4f4dabc612369b4e5235edd1cf
caljun <calderonjunya0602@gmail.com>
2025-05-04 00:36:22 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,17 +13,16 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+// 🔽 ← これを追加
+app.use('/uploads', express.static('uploads'));
+
 // ルート定義
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/location', require('./routes/location'));
+app.use('/api/friends', require('./routes/friend'));
+app.use('/api/areas', require('./routes/area'));
 
 // サーバー起動
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.use('/api/location', require('./routes/location'));
-
-app.use('/api/friends', require('./routes/friend'));
-
-app.use('/api/areas', require('./routes/area'));
-
 
