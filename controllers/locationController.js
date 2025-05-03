05421f7a3660024c72d7a5c01941682b6faa5085
caljun <calderonjunya0602@gmail.com>
2025-05-04 00:37:35 +0900

    Update locationController.js

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,30 +1,16 @@
-const Location = require('../models/Location');
-
-// 関学構内判定（長方形エリア）
-const isInsideCampus = (lat, lng) => {
-  return (
-    lat <= 34.7713160 && lat >= 34.7651447 &&
-    lng >= 135.3433948 && lng <= 135.3501509
-  );
-};
-
 exports.updateLocation = async (req, res) => {
   const { latitude, longitude } = req.body;
-  const userId = req.user.id; // JWTミドルウェアでセットされたユーザーID
+  const userId = req.user.id;
 
   try {
-    // 位置情報の保存 or 更新
     const location = await Location.findOneAndUpdate(
       { user: userId },
       { latitude, longitude, updatedAt: new Date() },
       { upsert: true, new: true }
     );
 
-    const insideCampus = isInsideCampus(latitude, longitude);
-
     res.status(200).json({
       message: '位置情報を更新しました',
-      insideCampus,
       location
     });
   } catch (err) {
