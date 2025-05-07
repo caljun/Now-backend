18abd62347e9d65914ac3fcd219a7a4805c3bf6e
caljun <calderonjunya0602@gmail.com>
2025-05-07 09:11:14 +0900

    Update friendController.js

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,14 +1,6 @@
 const User = require('../models/user');
 const Location = require('../models/Location');
 
-// 関学構内の範囲定義
-const isInsideCampus = (lat, lng) => {
-  return (
-    lat >= 34.788500 && lat <= 34.792500 &&
-    lng >= 135.351000 && lng <= 135.357500
-  );
-};
-
 // ① 友達を追加する処理
 exports.addFriend = async (req, res) => {
   const userId = req.user.id;
@@ -35,29 +27,6 @@ exports.addFriend = async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: '友達の追加に失敗しました' });
-  }
-};
-
-// ② 構内にいる友達を返す処理
-exports.getFriendsInCampus = async (req, res) => {
-  const userId = req.user.id;
-
-  try {
-    const user = await User.findById(userId).populate('friends');
-    const friendIds = user.friends.map(friend => friend._id);
-
-    const locations = await Location.find({ user: { $in: friendIds } });
-
-    const inCampusFriends = locations
-      .filter(loc => isInsideCampus(loc.latitude, loc.longitude))
-      .map(loc => loc.user);
-
-    const inCampusUsers = await User.find({ _id: { $in: inCampusFriends } });
-
-    res.json(inCampusUsers);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: '友達の構内確認に失敗しました' });
   }
 };
 
@@ -156,7 +125,10 @@ exports.acceptFriend = async (req, res) => {
     await user.save();
     await friend.save();
 
-    res.status(200).json({ message: '友達リクエストを承認しました' });
+    res.status(200).json({ 
+      message: '友達リクエストを承認しました' ,
+      friendNowId: friend.nowId
+  });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: '承認に失敗しました' });
